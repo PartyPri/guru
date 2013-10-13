42ff1dd15d7a58b04a1fb350c78e28105c63db0d
tshr <toshirosugihara@Toshiros-Mac-mini.local>
2013-10-13 12:14:53 -0700

    Generated devise views, removed middle_name field from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131013182835) do
+ActiveRecord::Schema.define(:version => 20131013190841) do
 
   create_table "interests", :force => true do |t|
     t.string   "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(:version => 20131013182835) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
-    t.string   "middle_name"
     t.string   "last_name"
   end
 
