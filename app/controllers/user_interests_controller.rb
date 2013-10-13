aa15d06efdfbea9b2bae0d8ac5aa80a109d2d622
tshr <toshirosugihara@Toshiros-Mac-mini.local>
2013-10-13 13:19:40 -0700

    user join with validations and flash messages on interest show

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,8 +1,13 @@
 class UserInterestsController < ApplicationController
   def create
     if user_signed_in?
-      UserInterest.create(user_id: current_user.id, interest_id: params[:id])
-      redirect_to :root
+      user_interest = UserInterest.new(user_id: current_user.id, interest_id: params[:id])
+      if user_interest.save
+        flash[:notice] = "Added"
+      else
+        flash[:error] = "You are already signed up!"
+      end
+      redirect_to interest_path(params[:id])
     end
   end
 end
