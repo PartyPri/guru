9b05190c3694301d7eeeec1e8f42b12ae7166f02
tshr <toshirosugihara@Toshiros-Mac-mini.local>
2013-10-19 12:55:43 -0700

    added workshop_interest belongs to associations, and interests list on workshop show

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ActiveRecord::Base
   has_many :user_interests
   has_many :interests, through: :user_interests, uniq: true
 
-
   # Validations
 
   validates :first_name, presence: true
