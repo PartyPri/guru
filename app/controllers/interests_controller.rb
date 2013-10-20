94cc1f90c15583efff966fe21f6e8f8c706ae72d
Priya J <pjhingan@gmail.com>
2013-10-20 07:34:03 -0700

    Added workshop list to interest show page. Converted interest to link on user show page. Changed 'Your calendar' to 'Activity' on home page.

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,5 +2,6 @@ class InterestsController < ApplicationController
   def show
     @interest = Interest.find(params[:id])
     @users = @interest.users
+    @workshops = @interest.workshops
   end
 end
--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -2,7 +2,7 @@ class WorkshopsController < ApplicationController
   def show
     @workshop = Workshop.where(id: params[:id]).first
     if @workshop.blank?
-      redirect_to :root#, error: "User could not be found"
+      redirect_to :root#, error: "Workshop could not be found"
     else
       @interests = @workshop.interests
     end
