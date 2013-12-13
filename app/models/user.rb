20ddcf23bcbc86242fd4ce7027547390a255ab63
Priya J <pjhingan@gmail.com>
2013-12-13 10:41:00 -0800

    Added settings link to navbar to enable user to change description. Changed shared header view file from '_header' to '_nav'. Updated application view to reflect this change. Added 'heading' stylesheet to allow sharing of heading styles between interest and user pages. Added '@import heading' to application css file. Added description field to user via migration. Cleaned up interest and user show pages; made follow button in menubar functional.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   # Access
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :description
 
   # Associations
 
