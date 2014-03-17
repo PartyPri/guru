b3155b5d81bc68ee0c0c9f0602caa8ccf36cc423
Priya J <pjhingan@gmail.com>
2014-03-17 13:56:52 -0700

    Updated landing page to incorporate partial email_contact form and updated styling of email_contact form.

--- app/controllers/email_contacts_controller.rb
+++ app/controllers/email_contacts_controller.rb
@@ -4,9 +4,9 @@ class EmailContactsController < ApplicationController
     @email_contact = EmailContact.new(params[:email_contact])
     if @email_contact.save
       @email_contact = EmailContact.new
-      render "pages/home", flash: { success: "Email stored" }
+      render "pages/landing", flash: { success: "Email stored" }
     else
-      render "pages/home", flash: { error: "Duplicate email" }#@email_contact.errors.first
+      render "pages/landing", flash: { error: "Duplicate email" }#@email_contact.errors.first
     end
   end
 end
