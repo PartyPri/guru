133708cf41fe47088554367f9faf11dc0b8b997b
ccshelton <ccshelton@gmail.com>
2015-04-01 16:10:37 -0700

    Added validation for form fields in the model and view, and included Stripe errors in view

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,5 +7,8 @@ class Registration < ActiveRecord::Base
 
   # Validations
   validates :availability_confirmation, :acceptance => {:message => "You must confirm that you are available for the event."}
+  validates :name, :email, length: {maximum: 50, message: "is too long"}
+  validates :phone, length: {maximum: 20, message: "is too long"}
+  validates :name, :email, :phone, :presence => {:message => "cannot be blank"}
 
 end
