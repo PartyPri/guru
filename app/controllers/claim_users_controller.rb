40c573b986802ca554ee99bc62cd661c69210140
ccshelton <ccshelton@gmail.com>
2015-04-16 19:56:10 -0700

    added redirect on successfull claim users email sent, and flash message

--- app/controllers/claim_users_controller.rb
+++ app/controllers/claim_users_controller.rb
@@ -8,5 +8,7 @@ class ClaimUsersController < ApplicationController
     @user = User.find_by_email(@email)
     @token = @user.claim_token
     ClaimUserMailer.claim_user(@email, @token).deliver
+    redirect_to '/'
+    flash[:success] = "Email sent!"
   end
 end
