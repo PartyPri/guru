754ba89ea184a99c7cab49477468eca3f403ca8f
tshr <toshiro.sugihara@gmail.com>
2015-04-29 02:31:22 -0700

    scope reels by interest

--- app/controllers/reels_controller.rb
+++ app/controllers/reels_controller.rb
@@ -1,7 +1,15 @@
 class ReelsController < ApplicationController
 
   def index
-    tagged_reels = Reel.tagged_with(params[:tag])
+    interest_id = params[:interest_id]
+
+    if interest_id && interest = Interest.find(interest_id)
+      reels = interest.reels
+    else
+      reels = Reel
+    end
+
+    tagged_reels = reels.tagged_with(params[:tag])
 
     if params[:user_id]
       tagged_reels = tagged_reels.by_user_id(params[:user_id].to_i)
