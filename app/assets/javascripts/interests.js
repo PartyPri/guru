e044206ed5efbe57c491d2f4b581d9b1781e9562
ccshelton <ccshelton@gmail.com>
2015-05-07 13:45:07 -0700

    added function to build new teasers based off of data returned from AJAX

--- app/assets/javascripts/interests.js
+++ app/assets/javascripts/interests.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#tag-select').change(function() {
-    tag = $(this).val();
+    //When dropdown changes, get it's value
+    var tag = $(this).val();
+
+    //Using tag, create query parameter for GET request
     var data = {'tag':tag};
 
     $.ajax({
@@ -9,22 +12,38 @@ $(document).ready(function() {
       data: data,
       dataType: 'json',
       success: function(response) {
-        var values = getReelInfo(response);
-        console.log(values);
+        console.log(response)
+
+        //Reset content of #reels div
+        $(".content-container#reels").html("");
+
+        //For each object in AJAX response, call getReelInfo function
+        //Append HTML from getReelInfo function to page
+        for (i = 0; i < response.length; i++) {
+          var reels = getReelInfo(response, i);
+          $(".content-container#reels").append(reels);
+        };
 
       }
     });
   });
 
-  function getReelInfo(reel) {
-    var names =[];
-    var reel_list = reel;
+  function getReelInfo(reel, num) {
+    //Get information we need about reel
+    var reel = reel[num]
+    var name = reel.name;
+    var description = reel.description
+    var id = reel.id
 
-    for (i = 0; i < reel_list.length; i++) {
-      var name = reel_list[i].name;
-      names.push(name);
+    for (r = 0; r < reel.images.length; r ++) {
+      var images = reel.images[r]
+      var img = "<li style='background-image: url("+images.photo+")' class='teaser-media'></li>"
     }
 
-    return names;
+    //Build teaser div with reel information
+    var html = "<div class='teaser'><a href='/reels/" + id + "'><h2>" + name + "</h2><ul class='list-inline'>" + img + "</ul><p>" + description + "</p></a></div>"
+
+    return html
+
   };
 });
