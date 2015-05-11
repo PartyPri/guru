b03ce40c108448e85ac40343b04d787f99db7859
ccshelton <ccshelton@gmail.com>
2015-05-10 20:16:02 -0700

    updated redirects for new images, new videos, and new reels

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,12 +2,13 @@ class ImagesController < ApplicationController
 
   def create
     if user_signed_in?
+      @reel = params[:media][:reel_id]
       @image = Image.new(reel_id: params[:media][:reel_id],
                          title: params[:title],
                          description: params[:description],
                          photo: params[:file] )  
       if @image.save
-        redirect_to current_user
+        redirect_to reel_path(@reel)
         flash[:notice] = "Image added!"
 
       else
