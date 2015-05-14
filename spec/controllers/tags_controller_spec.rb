dbb51b925949fbcb7bc0f0f0c81e1917973ac8c8
tshr <toshiro.sugihara@gmail.com>
2015-05-13 19:12:48 -0700

    user and tags controller spec

---
+++ spec/controllers/tags_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Api::TagsController, type: :controller do
+  describe "index" do
+    context "on success" do
+      before do
+        ['waacking', 'watersports', 'painting'].each do |tag|
+          ActsAsTaggableOn::Tag.new(name: tag).save
+        end
+        get :index, term: "wa"
+      end
+
+      it "returns 200 on success" do
+        expect(response.status).to eq 200
+      end
+
+      it "returns the matching tags" do
+        parsed_response = JSON(response.body)
+        expect(parsed_response).to match_array ["waacking", "watersports"]
+      end
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe User do
+  context "instance methods" do
+    describe "token_expired?" do
+      let (:user) {create(:user)}
+      it "returns true if the expires_at field is before now" do
+        user.expires_at = Time.now - 5.hours
+        expect(user.token_expired?).to eq true
+      end
+
+      it "returns true if the expires_at field is before now" do
+        user.expires_at = Time.now + 5.hours
+        expect(user.token_expired?).to eq false
+      end
+    end
+
+    describe "followed_users" do
+      # TODO but see note to refactor at method declaration
+    end
+  end
+
+  context "class methods" do
+    describe "find_for_google_oauth2" do
+      # TODO
+    end
+  end
+end
