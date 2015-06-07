ae3ffb5c97a5995e3ba3510ed33f0ac437d08539
ccshelton <ccshelton>
2015-06-06 18:27:39 -0700

    ensured that media form also saves images, styled the form to look like it used to, removed uneeded JS in new-media.js

--- app/assets/javascripts/new_media.js
+++ app/assets/javascripts/new_media.js
@@ -1,87 +1,59 @@
 $(document).ready(function() {
-  if($("#upload-token-path").length) {
-    var submit_button = $('#submit_pre_upload_form');
-    var media_upload = $('#media_upload');
-    //Get upload token from YouTube client
-    submit_button.click(function (event) {
-      event.preventDefault();
-      var filepath = $("#file").val();
-      var extension = getFileExtension(filepath);
-      //If no file extension on the file, cry for help.
-      if (extension == "") {
-        return alert("Please add an extension to your filename and upload again.");
-      }
-      //If this is a movie file supported by YouTube, post to YouTube
-      if (extension.match(/^(MOV|MPEG4|MP4|AVI|WMV|MPEGPS|FLV|3GPP|WEBM)$/i)) {
-        validate_and_upload(media_upload, submit_button);
-      }
-      // All other file types upload through paperclip
-      else {
-        upload_image(media_upload);
-      }
-    });
+  var submit_button = $('#submit-media')
+  var media_upload = $('#media_upload');
 
-    var upload_video = function(media_upload, submit_button) {
-      var upload_token_path = $("#upload-token-path").data("upload-token-path");
-      $.ajax({
-          type: 'POST',
-          url: upload_token_path,
-          data: $('#media_pre_upload').serialize(),
-          dataType: 'json',
-          success: function(response) {
-            //With YouTube upload token and video_uid_url, upload video to YouTube
-            media_upload.find('#token').val(response.token);
-            media_upload.attr('action', response.url.replace(/^http:/i, window.location.protocol)).submit();
-            submit_button.unbind('click').hide();
-            $('.preloader').css('display', 'block');
-          },
-          error: function (xhr) {
-            if(xhr.status == 401) {
-              alert("Your session has timed out. Please login again.");
-              var root_path = $("#root-path").data("root-path");
-              window.location.replace(root_path);
-              }
-            else {
-            //This is a common cause for YouTube upload errors:
-            alert("Something went wrong. Please visit www.youtube.com and create a channel if you do not already have one.");
-          }
-        }
-      });
+  submit_button.click(function (event) {
+    event.preventDefault();
+    var filepath = $("#video_file").val();
+    var extension = getFileExtension(filepath);
+    //If no file extension on the file, cry for help.
+    if (extension == "") {
+      return alert("Please add an extension to your filename and upload again.");
     }
+    //If this is a movie file supported by YouTube, post to YouTube
+    if (extension.match(/^(MOV|MPEG4|MP4|AVI|WMV|MPEGPS|FLV|3GPP|WEBM)$/i)) {
+      validate_and_upload(media_upload, submit_button);
+    }
+    // All other file types upload through paperclip
+    else {
+      upload_image(media_upload);
+    }
+  });
 
-    var validate_and_upload = function(media_upload, submit_button) {
-      if(($("#description").val() != "") && ($("#media_reel_id").val() != "")) {
-        upload_video(media_upload, submit_button);
-      }
-      else {
-        alert("Please fill out all form fields");
-      }
+  var validate_and_upload = function(media_upload, submit_button) {
+    if(($("#description").val() != "") && ($("#media_reel_id").val() != "")) {
+      upload_video(media_upload, submit_button);
     }
+    else {
+      alert("Please fill out all form fields");
+    }
+  }
 
-    
-    var upload_image = function(media_upload) {
-      var image_upload_url = $('#image-upload-url').data("image-upload-url"); 
-      combine_upload_forms();
-      media_upload.attr('action', image_upload_url).submit();
-    }
+  var upload_video = function(media_upload, submit_button) { 
+    media_upload.submit();
+  }
 
-    //Add hidden copies of the pre_media_form fields to the media_upload form. NOTE: these fields are separated in a separate form
-    //originalaly in order to create the YouTube upload token in the case of a video upload. See above.
-    var combine_upload_forms = function() {
-      var pre_media_form_groups = $("#media_pre_upload").children(".form-group").clone();
-      pre_media_form_groups.children().removeAttr('required');
-      pre_media_form_groups.hide();
-      $("#upload_fields").prepend(pre_media_form_groups);
-      $("#upload_fields #description").val($("#description").val()); //The description val, mysteriously, isn't copied.
-      $("#upload_fields #media_reel_id").val($("#media_reel_id").val())
-    }
+  var upload_image = function(media_upload) {
+    var image_upload_url = $('#image-upload-url').data("image-upload-url");
+    media_upload.attr('action', image_upload_url).submit();
+  }
 
-    var getFileExtension = function(filename) {
-      var a = filename.split(".");
-      if( a.length === 1 || ( a[0] === "" && a.length === 2 ) ) {
-          return "";
-      }
-      return a.pop();
+  //Add hidden copies of the pre_media_form fields to the media_upload form. NOTE: these fields are separated in a separate form
+  //originalaly in order to create the YouTube upload token in the case of a video upload. See above.
+  // var combine_upload_forms = function() {
+  //   var pre_media_form_groups = $("#media_pre_upload").children(".form-group").clone();
+  //   pre_media_form_groups.children().removeAttr('required');
+  //   pre_media_form_groups.hide();
+  //   $("#upload_fields").prepend(pre_media_form_groups);
+  //   $("#upload_fields #description").val($("#description").val()); //The description val, mysteriously, isn't copied.
+  //   $("#upload_fields #media_reel_id").val($("#media_reel_id").val())
+  // }
+
+  var getFileExtension = function(filename) {
+    var a = filename.split(".");
+    if( a.length === 1 || ( a[0] === "" && a.length === 2 ) ) {
+        return "";
     }
+    return a.pop();
   }
 });
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,19 +12,21 @@ class VideosController < ApplicationController
   end
 
   def create
-    @video = Video.new(description: params[:video][:description], reel_id: params[:video][:reel_id])
+    check_valid_session
+
     @reel = Reel.find(params[:video][:reel_id])
 
-    if @reel.videos.length == 0
-      @video.title = "#{@reel.name} - 1"
-    else
-      @video.title = "#{@reel.name} - #{@reel.videos.length + 1}"
-    end
+    @video = Video.new(description: params[:video][:description], reel_id: params[:video][:reel_id])
+  
 
     if @video.save
+      @video.title = "#{@reel.name} - #{@video.id}"
 
-      file = Yt::Account.new access_token: current_user.token
-      file.upload_video params[:video][:file].try(:tempfile).try(:to_path), title: @video.title, description: @video.description, category: 'Entertainment'
+      account = Yt::Account.new access_token: current_user.token
+      file = account.upload_video params[:video][:file].try(:tempfile).try(:to_path), title: @video.title, description: @video.description, category: 'Entertainment'
+      
+      @video.uid = file.id
+      @video.save!
 
       flash[:success] = 'Your video has been uploaded!'
       redirect_to @reel
