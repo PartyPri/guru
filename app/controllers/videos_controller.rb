de24fcf506d1457a4af2c5b01d652df180e5c1b3
ccshelton <ccshelton>
2015-06-07 17:27:42 -0700

    added flash message for no youtube channel error

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -13,26 +13,28 @@ class VideosController < ApplicationController
 
   def create
     check_valid_session
+    account = Yt::Account.new access_token: current_user.token
 
-    @reel = Reel.find(params[:video][:reel_id])
-
-    @video = Video.new(description: params[:video][:description], reel_id: params[:video][:reel_id])
-  
-
-    if @video.save
-      @video.title = "#{@reel.name} - #{@video.id}"
-
-      account = Yt::Account.new access_token: current_user.token
-      file = account.upload_video params[:video][:file].try(:tempfile).try(:to_path), title: @video.title, description: @video.description, category: 'Entertainment'
-      
-      @video.uid = file.id
-      @video.save!
-
-      flash[:success] = 'Your video has been uploaded!'
-      redirect_to @reel
-    else
-      flash[:error] = 'Something went wrong'
+    if !account.channel.public?
+      flash[:error] = 'Your channel is not public! <a href="https://www.youtube.com/signin?next=/create_channel" target="_blank">Click here</a> to turn it on, and then retry your upload.'.html_safe
       render :new
+    else
+      @reel = Reel.find(params[:video][:reel_id])
+      @video = Video.new(description: params[:video][:description], reel_id: params[:video][:reel_id])
+  
+      if @video.save
+        @video.title = "#{@reel.name} - #{@reel.videos.length}"
+
+        file = account.upload_video params[:video][:file].try(:tempfile).try(:to_path), title: @video.title, description: @video.description, category: 'Entertainment'
+        @video.uid = file.id
+        @video.save!
+
+        flash[:success] = 'Your video has been uploaded!'
+        redirect_to @reel
+      else
+        flash[:error] = 'Something went wrong'
+        render :new
+      end
     end
   end
 
