dfd37ac596eee58d9463ad5e72c35491c9258d29
ccshelton <ccshelton>
2015-06-18 17:29:37 -0700

    styled the edit modal and made sure it clears all content when clicking 'cancel' or outside of the modal

--- app/assets/javascripts/edit-modal.js
+++ app/assets/javascripts/edit-modal.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
     $('.modal-edit-media').hide();
     $('#modal-edit-' + $(this).attr('data-medium')).show();
 
+    $('.btn-cancel').click(function() {
+      $('#edit-modal').modal('hide');
+    });
+  });
+
+  $('#edit-modal').on('hide.bs.modal', function(e) {
+    $('form.edit_reel').trigger("reset");
   });
 });
