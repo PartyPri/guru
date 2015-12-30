2d2f4262cb7ac571911e790170c7b90ca19a7608
Jane Sternbach <janesternbach@gmail.com>
2015-12-29 17:23:44 -0800

    interests instance var for creating/editing reels

--- spec/controllers/reels_controller_spec.rb
+++ spec/controllers/reels_controller_spec.rb
@@ -2,13 +2,28 @@ require 'spec_helper'
 
 describe ReelsController, :type => :controller do
 
-  let!(:reel) {create(:reel) }
+  let!(:reel) { create(:reel) }
 
   describe "GET #create" do
-
     it 'should create a reel' do
       post :create, post: reel
       expect(response.status).to eq(302)
+    end
+  end
+
+  describe 'GET #new' do
+    let(:interests) { create(:interest) }
+    it 'assigns an interests instance var' do
+      get :new
+      expect(assigns(:interests)).to eq [interests]
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:interests) { create(:interest) }
+    it 'assigns an interests instance var' do
+      get :edit, id: reel.id
+      expect(assigns(:interests)).to eq [interests]
     end
   end
 
@@ -20,10 +35,16 @@ describe ReelsController, :type => :controller do
   end
 
   describe "GET #show" do
-
     it 'should find a reel' do
       found_reel = Reel.where(name: 'Foo Reel')
       expect(found_reel).to exist
+    end
+  end
+
+  describe 'set_interests' do
+    it 'includes tags in the query' do
+      expect(Interest).to receive(:includes).with(:tags)
+      controller.send(:set_interests)
     end
   end
 end
