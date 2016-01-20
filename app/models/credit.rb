c69b865dcaa7c63e990f6ebbbf044fb93e1305a3
Jane Sternbach <janesternbach@gmail.com>
2016-01-19 19:08:57 -0800

    JS enum functionality for invitation_status for credit

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,7 +1,17 @@
 class Credit < ActiveRecord::Base
-  attr_accessible :accepted_invitation, :credit_receiver_email, :credit_receiver_id, :reel_id, :reel_owner_id, :role
+  include EnumHelper
+
+  attr_accessible :invitation_status, :credit_receiver_email, :credit_receiver_id, :reel_id, :reel_owner_id, :role, :pending, :accepted, :rejected
 
   belongs_to :reel
+
+  # The `enum` method here comes from the EnumHelper module. It gives you a few helper methods:
+  #
+  # Credit.invitation_status_states => the enum value for each state
+  # Credit.new.pending? => boolean value of whether that state is the current one selected
+  # Credit.new.pending= => sets the value of invitation_status to the state being set
+  #
+  enum(:invitation_status, :pending, :accepted, :rejected)
 
   validates_presence_of :credit_receiver_email, :reel_id, :reel_owner_id, :role
 end
