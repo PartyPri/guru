fd0afaee0239f8b8d774b41b8de444513cf9841d
Jane Sternbach <janesternbach@gmail.com>
2016-02-14 00:58:13 -0800

    url methods to video

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,12 @@ class Video < Medium
   attr_accessible :description, :project_id, :uid
 
   validates_presence_of :description
+
+  def screenshot_url
+    "http://img.youtube.com/vi/#{uid}/0.jpg"
+  end
+
+  def embed_url
+    "https://www.youtube.com/embed/#{uid}"
+  end
 end
