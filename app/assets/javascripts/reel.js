7ff3920107217a5e9767785e196049e9dd6c1d3b
teckliew <bboytek@gmail.com>
2016-02-29 13:54:39 -0800

    fix tagging toggle

--- app/assets/javascripts/reel.js
+++ app/assets/javascripts/reel.js
@@ -21,8 +21,8 @@ $(document).ready(function() {
     var newTagElement = function(interest, interest_id, tag){
       var tagPill = document.createElement("div");
       var tagIcon = document.createElement("i");
-      var tagName = document.createTextNode(" "+tag)
-      tagPill.className = "tag-disable " + interest;
+      var tagName = document.createTextNode(" " + tag);
+      tagPill.className = "tag-disable " + escChar(interest.split(' ').join(''));
       tagIcon.className = "fa fa-tag";
       tagPill.setAttribute("value", tag);
       tagPill.appendChild(tagIcon);
@@ -42,9 +42,10 @@ $(document).ready(function() {
 
     //selecting and deselecting interest
     $('.interest-check').change(function(){
-      var interest = $(this)[0].nextSibling.nodeValue.trim();
+      var interest = escChar($(this)[0].nextSibling.nodeValue.trim().split(' ').join(''));
       $('.'+interest).toggleClass('tag-disable');
       $('.'+interest).toggleClass('tag');
+      $('.'+interest).toggleClass('selectable');
       $('.'+interest).removeClass('tag-selected');
       tagSelected();
     });
@@ -52,18 +53,25 @@ $(document).ready(function() {
     //refreshes list of available tags
     $(document).click(function(){
       //selecting and deselecting tags
-      $('.tag').click(function(){
-        $(this).toggleClass('tag-selected');
-        tagSelected();
+      $('.selectable').click(function(){
+        if($(this).hasClass('selectable')){
+          $(this).toggleClass('tag-selected');
+          tagSelected();
+        }
       });
     });
 
-    //set selected tags
+    //set selected tags for the form
     function tagSelected(){
       var tagList = $('.tag-selected').text();
       tagList = tagList.split(" ");
       tagList = tagList.join(", ");
       $('#reel_tag_list').val(tagList.slice(2));
+    }
+
+    //escapes special characters for jquery selectors
+    function escChar(string) {
+        return string.replace(/[!"#$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/g, "")
     }
   }
 
