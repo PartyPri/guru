2c579dd57b7a8d0b7fa5f3524a405d1e51becf1c
Toshiro Sugihara <toshirosugihara@Toshiros-Mac-mini.local>
2016-03-22 03:13:58 -0700

    update integration test for design changes and refactor

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -14,20 +14,13 @@ describe "the login process" do
 
     create_reel
 
-    add_media("Image", "test.jpeg")
+    add_image
     expect(page).to have_content "Image added!"
 
-    add_media('Video', "test.mp4")
+    add_video
     expect(page).to have_content "Your video has been uploaded!"
 
-    # Add story
-    click_link("ADD MEDIA")
-    click_link("Story")
-    attach_file('story_photo', Rails.root.to_path + "/spec/fixtures/test.jpeg")
-    fill_in 'story_title', with: "Test Story"
-    fill_in_ckeditor('story_body', with: "Test story body")
-    click_button('Save')
-
+    add_story
     expect(page).to have_content "Test Reel"
   end
 end
@@ -50,17 +43,45 @@ def login_via_google
 end
 
 def create_reel
-  click_link("CREATE REEL")
+  click_link("Share Your Passion")
+  click_link("New Reel")
   fill_in 'reel_name', :with => 'Test Reel'
   fill_in 'reel_description', :with => 'Test reel description'
   click_button('Save')
 end
 
+def go_to_add_media
+  click_link("Share Your Passion")
+  click_link("Add to Reel")
+end
+
 def add_media(link, fixture)
-  click_link("ADD MEDIA")
+  go_to_add_media
   click_link(link)
-  page.execute_script("$('#video_file').toggle();")
+  show_file_input("video_file")
   attach_file('video_file', Rails.root.to_path + "/spec/fixtures/" + fixture)
   fill_in 'video_description', with: "Test description"
   click_button('submit-media')
+end
+
+def show_file_input(id)
+  page.execute_script("$('##{id}').toggle();")
+end
+
+def add_image
+  add_media("Image", "test.jpeg")
+end
+
+def add_video
+  add_media('Video', "test.mp4")
+end
+
+def add_story
+  go_to_add_media
+  click_link("Story")
+  show_file_input("story_photo")
+  attach_file('story_photo', Rails.root.to_path + "/spec/fixtures/test.jpeg")
+  fill_in 'story_title', with: "Test Story"
+  fill_in_ckeditor('story_body', with: "Test story body")
+  click_button('Save')
 end
