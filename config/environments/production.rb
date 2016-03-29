690fcc0cf9e66426093bad4b91d398734205dbca
Priya J <pjhingan@gmail.com>
2016-03-29 10:33:43 -0700

    precompile another active admin asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Guru::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += [ 'masonry.pkgd.js', 'imagesloaded.pkgd.js', 'projects.js' ]
   config.assets.precompile += %w{landing.css}
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css active_admin.js active_admin/print.css]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
