adefae4b0eb9df946065d65ae825f068b440c0c8
teckliew <bboytek@gmail.com>
2016-04-17 02:48:08 -0700

    multiple upload for images with dropzone.js

--- app/assets/javascripts/new_media.js
+++ app/assets/javascripts/new_media.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var submit_button = $('#submit-media')
+  var submit_button = $('#submit-media');
   var media_upload = $('#media_upload');
 
   submit_button.click(function (event) {
@@ -7,7 +7,7 @@ $(document).ready(function() {
     var filepath = $("#video_file").val();
     var extension = getFileExtension(filepath);
     //If no file extension on the file, cry for help.
-    if (extension == "") {
+    if (extension === "") {
       return alert("Please add an extension to your filename and upload again.");
     }
     //If this is a movie file supported by YouTube, post to YouTube
@@ -23,20 +23,20 @@ $(document).ready(function() {
   var upload_video = function(media_upload, submit_button) {
     $('.preloader').show();
     media_upload.submit();
-  }
+  };
 
   var upload_image = function(media_upload) {
     $('.preloader').show();
     var image_upload_url = $('#image-upload-url').data("image-upload-url");
 
-    if(($("#video_description").val() != "") && ($("#video_reel_id").val() != "")) {
+    if(($("#video_description").val() !== "") && ($("#video_reel_id").val() !== "")) {
       media_upload.attr('action', image_upload_url).submit();
     }
 
     else {
       alert("Please fill out all form fields");
     }
-  }
+  };
 
   var getFileExtension = function(filename) {
     var a = filename.split(".");
@@ -44,14 +44,50 @@ $(document).ready(function() {
         return "";
     }
     return a.pop();
-  }
+  };
 
   var validate_and_upload = function(media_upload, submit_button) {
-    if(($("#video_description").val() != "") && ($("#video_reel_id").val() != "")) {
+    if(($("#video_description").val() !== "") && ($("#video_reel_id").val() !== "")) {
       upload_video(media_upload, submit_button);
     }
     else {
       alert("Please fill out all form fields");
     }
-  }
+  };
+
+  //changing finished link based on reel selected
+  $("#video_reel_id").change(function(){
+    var reelNum = $("#video_reel_id").val();
+    console.log(reelNum);
+    $("#image-new-finish").attr("href", "../reels/" + reelNum);
+  });
+
+  $("#media_upload").dropzone({
+    paramName: "video[file]",
+    url: "../images",
+    method: "post",
+    addRemoveLinks: true,
+    success: function(file, response){
+			// find the remove button link of the uploaded file and give it an id
+			// based of the fileID response from the server
+			$(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
+			// add the dz-success class (the green tick sign)
+			$(file.previewElement).addClass("dz-success");
+		},
+    //when the remove button is clicked
+    removedfile: function(file){
+      // grap the id of the uploaded file we set earlier
+      var id = $(file.previewTemplate).find('.dz-remove').attr('id');
+
+      // make a DELETE ajax request to delete the file
+      $.ajax({
+        type: 'DELETE',
+        url: '../images/' + id,
+        success: function(data){
+          console.log(data.message);
+          $(file.previewTemplate).find('.dz-remove').parent().remove();
+        }
+      });
+    }
+  });
 });
