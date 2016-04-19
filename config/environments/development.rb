521858b36027671a11a0378e148a378c591e22ca
Jane Sternbach <janesternbach@gmail.com>
2016-04-19 00:11:51 -0700

    configure either sending email or pop up tab

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,9 @@ Guru::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :letter_opener
+  if ENV['SEND_EMAILS'] != "1"
+    config.action_mailer.delivery_method = :letter_opener
+  end
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
